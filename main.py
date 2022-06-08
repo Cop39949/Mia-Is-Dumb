c9ca8ac8fc0b97e58df95fe6e93cee95a1d5f391
ThatHurt <102971602+Cop39949@users.noreply.github.com>
2022-06-08 15:12:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+import time
+import sys
+def typer(write):
+  for i in write:
+    sys.stdout.write(i)
+    sys.stdout.flush()
+    time.sleep(0.3)
+typer("Who let the dogs out")
+list1=[2,2,3,2,2,2]
+list1=[2,3,2,2,2,2]
+list1=[2,2,3,4,2,2]
+list1=[2,3,2,2,2,2]
+print(list1)
